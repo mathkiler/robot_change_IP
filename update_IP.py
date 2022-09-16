78969c67adc6bdd1c9670085da78ec071a7d5743
unknown <mathieu3577aversenc@gmail.com>
2022-09-16 16:51:18 +0200

    updatz

--- update_IP.py
+++ update_IP.py
@@ -1,22 +1,23 @@
 import requests
 import os
 
-def get_next_line(text, ind) :
-    try :
-        while True :
-            if text[ind] == "\n" :
-                return ind
-            ind+=1
-    except :
-        return 0
+
 
 
 html = requests.get("http://monip.org/")
 tex = html.text
-new_ip = tex[tex.find('<P ALIGN="center"><FONT size=8><BR>IP : ')+40:get_next_line(tex, tex.find('<P ALIGN="center"><FONT size=8><BR>IP : '))-156]
+new_ip_tamp = tex[tex.find('<P ALIGN="center"><FONT size=8><BR>IP : ')+40:tex.find('<P ALIGN="center"><FONT size=8><BR>IP : ')+55]
+new_ip = ""
+for k in range(len(new_ip_tamp)) :
+    if new_ip_tamp[k] in ".0123456789" :
+        new_ip+=new_ip_tamp[k]
+
+print(new_ip)
 f = open("./IP.txt", "w")
 f.write(new_ip)
 f.close()
+
+
 
 os.system('git add "IP.txt"')
 os.system('git commit -m "IP.txt"')
